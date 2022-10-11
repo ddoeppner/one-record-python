12d90db4ad3d60b1008f9652a5311bfb1cdd2348
Daniel A. Doeppner <7744572+ddoeppner@users.noreply.github.com>
2022-10-11 19:33:52 +0200

    changed logging format

--- onerecord/__init__.py
+++ onerecord/__init__.py
@@ -4,7 +4,7 @@ from onerecord.client import ONERecordClient
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s.%(funcName)s:%(lineno)d -  %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
